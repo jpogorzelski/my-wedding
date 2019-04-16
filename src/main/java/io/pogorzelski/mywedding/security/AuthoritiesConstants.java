9c31109062c70a3dd9da6aa4e1a927baf08dfba3
Jakub Pogorzelski <jakub.pogorzelski@cronn.de>
2019-04-16 11:43:35 +0200

    Add new roles

--- src/main/java/io/pogorzelski/mywedding/security/AuthoritiesConstants.java
+++ src/main/java/io/pogorzelski/mywedding/security/AuthoritiesConstants.java
@@ -11,6 +11,12 @@ public final class AuthoritiesConstants {
 
     public static final String ANONYMOUS = "ROLE_ANONYMOUS";
 
+    public static final String CUSTOMER = "ROLE_CUSTOMER";
+
+    public static final String COMPANY_OWNER = "ROLE_COMPANY_OWNER";
+
+    public static final String COMPANY_WORKER = "ROLE_COMPANY_WORKER";
+
     private AuthoritiesConstants() {
     }
 }
