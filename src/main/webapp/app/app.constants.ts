f6ae8817863815765977d705b1fbe2d0209abce6
Jakub Pogorzelski <qba.pogorzelski@gmail.com>
2019-05-18 14:37:54 +0200

    Add branch and build date to footer

--- src/main/webapp/app/app.constants.ts
+++ src/main/webapp/app/app.constants.ts
@@ -6,3 +6,5 @@ export const VERSION = process.env.VERSION;
 export const DEBUG_INFO_ENABLED: boolean = !!process.env.DEBUG_INFO_ENABLED;
 export const SERVER_API_URL = process.env.SERVER_API_URL;
 export const BUILD_TIMESTAMP = process.env.BUILD_TIMESTAMP;
+export const BUILD_DATE = process.env.BUILD_DATE;
+export const BRANCH = process.env.BRANCH;
--- src/main/webapp/app/layouts/footer/footer.component.ts
+++ src/main/webapp/app/layouts/footer/footer.component.ts
@@ -1,7 +1,11 @@
 import { Component } from '@angular/core';
+import { BUILD_DATE, BRANCH } from 'app/app.constants';
 
 @Component({
     selector: 'jhi-footer',
     templateUrl: './footer.component.html'
 })
-export class FooterComponent {}
+export class FooterComponent {
+    buildDate = BUILD_DATE;
+    branch = BRANCH;
+}
